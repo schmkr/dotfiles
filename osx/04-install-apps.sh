67e4ab40accc322160e0dd834fb45183d95d861d
schmkr <git@schmkr.nl>
2014-07-21 12:58:28 +0200

    Added the installation of iTerm2

--- osx/04-install-apps.sh
+++ osx/04-install-apps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-sh apps/dropbox.sh
-sh apps/google-chrome-canary.sh
-sh apps/evernote.sh
+sh apps/iterm2.sh;
+sh apps/dropbox.sh;
+sh apps/google-chrome-canary.sh;
+sh apps/evernote.sh;
---
+++ osx/apps/iterm2.sh
@@ -0,0 +1,7 @@
+http --download https://iterm2.com/downloads/stable/iTerm2_v2_0.zip
+
+tar -xf iTerm2*.zip;
+
+mv iTerm.app /tmp;
+
+rm iTerm2*.zip;
