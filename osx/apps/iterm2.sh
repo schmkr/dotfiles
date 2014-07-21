37bd7172e4eb319c4c35e290703474efd4bfc806
schmkr <git@schmkr.nl>
2014-07-21 23:11:07 +0200

    Updated iTerm2 installation to use variables

--- osx/apps/iterm2.sh
+++ osx/apps/iterm2.sh
@@ -1,7 +1,8 @@
-http --download https://iterm2.com/downloads/stable/iTerm2_v2_0.zip
+DOWNLOAD_URL="https://iterm2.com/downloads/stable/iTerm2_v2_0.zip";
+DOWNLOADED_FILE="iTerm2*.zip";
+APP_NAME="iTerm.app";
 
-tar -xf iTerm2*.zip;
-
-mv iTerm.app /Applications;
-
-rm iTerm2*.zip;
+http --download ${DOWNLOAD_URL};
+tar -xf ${DOWNLOADED_FILE};
+mv "${APP_NAME}" /Applications;
+rm ${DOWNLOADED_FILE};
