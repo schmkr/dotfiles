33f6d1a1bb19589c63d9790110ced1ac7d80888d
schmkr <git@schmkr.nl>
2014-07-22 12:03:37 +0200

    Updated the scripts that will install app apps

--- osx/04-install-apps.sh
+++ osx/04-install-apps.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
+sh apps/alfred-app.sh;
 sh apps/iterm2.sh;
 sh apps/dropbox.sh;
 sh apps/google-chrome-canary.sh;
+sh apps/hipchat.sh;
+sh apps/magicprefs.sh;
 sh apps/sublime-text.sh;
 sh apps/sequel-pro.sh;
-
+sh apps/virtualbox.sh;
+sh apps/vagrant.sh;
