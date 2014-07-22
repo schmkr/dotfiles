623bc7473483e083b238c949fe4aa9f89a3a8a8a
schmkr <git@schmkr.nl>
2014-07-22 11:09:43 +0200

    Updated dropbox.sh to use variables
    More in line with the other installation scripts

--- osx/apps/dropbox.sh
+++ osx/apps/dropbox.sh
@@ -1,8 +1,10 @@
-http --download https://www.dropbox.com/download?plat=mac;
+DOWNLOAD_URL="https://www.dropbox.com/download?plat=mac";
+DOWNLOADED_FILE="DropboxInstaller.dmg";
 
-open DropboxInstaller.dmg;
+http --download ${DOWNLOAD_URL};
+INSTALL_DISK=$(hdiutil attach ${DOWNLOADED_FILE} | tail -n1 | head -n1);
 
-sleep 3s;
+open -W /Volumes/Dropbox\ Installer/Dropbox.app;
 
-open /Volumes/Dropbox\ Installer/Dropbox.app;
-
+hdiutil detach ${INSTALL_DISK:0:15};
+rm ${DOWNLOADED_FILE};
