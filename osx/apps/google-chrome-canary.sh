52c4ee9f329e0b1ffc1d164d4c520d7421dd8f48
schmkr <git@schmkr.nl>
2014-07-22 12:01:00 +0200

    Updated Google Chrome Canary installation script

--- osx/apps/google-chrome-canary.sh
+++ osx/apps/google-chrome-canary.sh
@@ -1,11 +1,10 @@
-http --download https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg
+DOWNLOAD_URL="https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg";
+DOWNLOADED_FILE="GoogleChromeCanary.dmg";
 
-open GoogleChromeCanary.dmg
-
-sleep 10s;
+http --download ${DOWNLOAD_URL};
+INSTALL_DISK=$(hdiutil attach ${DOWNLOADED_FILE} | tail -n1 | head -n1);
 
 cp -r /Volumes/Google\ Chrome\ Canary/Google\ Chrome\ Canary.app /Applications/;
 
-umount /Volumes/Google\ Chrome\ Canary;
-
-rm GoogleChromeCanary.dmg
+hdiutil detach ${INSTALL_DISK:0:15};
+rm ${DOWNLOADED_FILE};
