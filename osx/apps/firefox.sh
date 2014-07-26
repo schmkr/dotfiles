7ab95189d0d79843fae28f3c0c57ec6320e047fb
schmkr <git@schmkr.nl>
2014-07-26 16:25:09 +0200

    Added installation script for Firefox

---
+++ osx/apps/firefox.sh
@@ -0,0 +1,13 @@
+DOWNLOAD_URL="https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US";
+DOWNLOADED_FILE="Firefox*.dmg";
+
+http --download ${DOWNLOAD_URL};
+INSTALL_DISK=$(hdiutil attach -noautoopen -noidme -nobrowse -mountpoint ./here ${DOWNLOADED_FILE} | tail -n1 | head -n1);
+echo "Mounted dmg to ${INSTALL_DISK:0:12}"
+
+cp -r ./here/Firefox.app /Applications;
+echo "Copied Firefox.app to /Applications";
+
+hdiutil detach ${INSTALL_DISK:0:12};
+rm ${DOWNLOADED_FILE};
+echo "Done";
