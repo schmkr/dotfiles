6019ec1ffc8996086650ba28310389aec8b58255
schmkr <git@schmkr.nl>
2014-07-26 16:24:36 +0200

    Updated dropbox.sh to use relative mountpoint

--- osx/apps/dropbox.sh
+++ osx/apps/dropbox.sh
@@ -2,9 +2,9 @@ DOWNLOAD_URL="https://www.dropbox.com/download?plat=mac";
 DOWNLOADED_FILE="DropboxInstaller.dmg";
 
 http --download ${DOWNLOAD_URL};
-INSTALL_DISK=$(hdiutil attach ${DOWNLOADED_FILE} | tail -n1 | head -n1);
+INSTALL_DISK=$(hdiutil attach -noautoopen -noidme -nobrowse -mountpoint ./here ${DOWNLOADED_FILE} | tail -n1 | head -n1);
 
-open -W /Volumes/Dropbox\ Installer/Dropbox.app;
+open -W ./here/Dropbox.app;
 
 hdiutil detach ${INSTALL_DISK:0:15};
 rm ${DOWNLOADED_FILE};
