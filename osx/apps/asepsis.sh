b0c923debc646aae533be9ec1df4edf416b30f64
schmkr <git@schmkr.nl>
2014-07-26 16:23:53 +0200

    Updated Asepsis installation script to use relative mountpoint

--- osx/apps/asepsis.sh
+++ osx/apps/asepsis.sh
@@ -2,9 +2,9 @@ DOWNLOAD_URL="http://downloads.binaryage.com/Asepsis-1.4.1.dmg" ;
 DOWNLOADED_FILE="Asepsis*.dmg";
 
 http --download ${DOWNLOAD_URL};
-INSTALL_DISK=$(hdiutil attach ${DOWNLOADED_FILE} | tail -n1 | head -n1);
+INSTALL_DISK=$(hdiutil attach -noautoopen -noidme -nobrowse -mountpoint ./here ${DOWNLOADED_FILE} | tail -n1 | head -n1);
 
-open -W /Volumes/Asepsis/Asepsis.pkg;
+open -W ./here/Asepsis.pkg;
 
 hdiutil detach ${INSTALL_DISK:0:15};
 rm ${DOWNLOADED_FILE};
