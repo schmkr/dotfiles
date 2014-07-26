777a2491ef48edbc4ef1cfe6c9c3a8d754c6da2e
schmkr <git@schmkr.nl>
2014-07-26 16:40:43 +0200

    Updated virtualbox to use relative mountpoint

--- osx/apps/virtualbox.sh
+++ osx/apps/virtualbox.sh
@@ -3,12 +3,12 @@ DOWNLOADED_FILE="VirtualBox-*.dmg";
 MOUNT_POINT="virtualbox";
 http --download ${DOWNLOAD_URL};
 
-INSTALL_DISK=$(hdiutil attach ${DOWNLOADED_FILE} | tail -n1 | head -n1);
+INSTALL_DISK=$(hdiutil attach -noautoopen -noidme -nobrowse -mountpoint ./here ${DOWNLOADED_FILE} | tail -n1 | head -n1);
 echo "Mounted dmg to ${INSTALL_DISK:0:12}"
 
 # Open pkg file and tell open to wait
 echo "Please run the wizard that will prompt shortly";
-open -W /Volumes/VirtualBox/VirtualBox.pkg;
+open -W ./here/VirtualBox.pkg;
 
 hdiutil detach ${INSTALL_DISK:0:12};
 rm ${DOWNLOADED_FILE};
