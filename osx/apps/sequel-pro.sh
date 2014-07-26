c2efb5b90498272ab3640007c69b951e44c4bf31
schmkr <git@schmkr.nl>
2014-07-26 16:38:59 +0200

    Updated sequel pro installation to use relative mountpoint

--- osx/apps/sequel-pro.sh
+++ osx/apps/sequel-pro.sh
@@ -2,10 +2,10 @@ DOWNLOAD_URL="https://sequel-pro.googlecode.com/files/sequel-pro-1.0.2.dmg";
 DOWNLOADED_FILE="sequel-pro*.dmg";
 
 http --download ${DOWNLOAD_URL};
-INSTALL_DISK=$(hdiutil attach ${DOWNLOADED_FILE} | tail -n1 | head -n1);
+INSTALL_DISK=$(hdiutil attach -noautoopen -noidme -nobrowse -mountpoint ./here ${DOWNLOADED_FILE} | tail -n1 | head -n1);
 
 # we need sudo for copying, because of some .svn files in the app :(
-sudo cp -r /Volumes/Sequel\ Pro*/Sequel\ Pro.app /Applications/;
+sudo cp -r ./here/Sequel\ Pro.app /Applications/;
 
 hdiutil detach ${INSTALL_DISK:0:15};
 rm ${DOWNLOADED_FILE};
