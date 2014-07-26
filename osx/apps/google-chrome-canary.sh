28551b5ca0933fd3281f21a5d9732636d0dbbb4a
schmkr <git@schmkr.nl>
2014-07-26 16:38:27 +0200

    Updated google chrome installation to use relative mountpoint

--- osx/apps/google-chrome-canary.sh
+++ osx/apps/google-chrome-canary.sh
@@ -2,9 +2,9 @@ DOWNLOAD_URL="https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dm
 DOWNLOADED_FILE="GoogleChromeCanary.dmg";
 
 http --download ${DOWNLOAD_URL};
-INSTALL_DISK=$(hdiutil attach ${DOWNLOADED_FILE} | tail -n1 | head -n1);
+INSTALL_DISK=$(hdiutil attach -noautoopen -noidme -nobrowse -mountpoint ./here ${DOWNLOADED_FILE} | tail -n1 | head -n1);
 
-cp -r /Volumes/Google\ Chrome\ Canary/Google\ Chrome\ Canary.app /Applications/;
+cp -r ./here/Google\ Chrome\ Canary.app /Applications/;
 
 hdiutil detach ${INSTALL_DISK:0:15};
 rm ${DOWNLOADED_FILE};
