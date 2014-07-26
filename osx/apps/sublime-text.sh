f07ec25da0f79da7ce661a65784a7b3eb28f8dc0
schmkr <git@schmkr.nl>
2014-07-26 16:39:30 +0200

    Updated sublime-text.sh to use relative mountpoint

--- osx/apps/sublime-text.sh
+++ osx/apps/sublime-text.sh
@@ -2,9 +2,9 @@ DOWNLOAD_URL="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.d
 DOWNLOADED_FILE="Sublime*.dmg";
 
 http --download ${DOWNLOAD_URL};
-INSTALL_DISK=$(hdiutil attach ${DOWNLOADED_FILE} | tail -n1 | head -n1);
+INSTALL_DISK=$(hdiutil attach -noautoopen -noidme -nobrowse -mountpoint ./here ${DOWNLOADED_FILE} | tail -n1 | head -n1);
 
-cp -r /Volumes/Sublime\ Text/Sublime\ Text.app /Applications/;
+cp -r ./here/Sublime\ Text.app /Applications/;
 
 hdiutil detach ${INSTALL_DISK:0:15};
 rm ${DOWNLOADED_FILE};
