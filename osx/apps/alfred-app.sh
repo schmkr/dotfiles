aecbbbcab1c7c9824e40612f6316d25a09edd308
schmkr <git@schmkr.nl>
2016-07-06 08:53:35 +0200

    update app name

--- osx/apps/alfred-app.sh
+++ osx/apps/alfred-app.sh
@@ -1,6 +1,6 @@
 DOWNLOAD_URL="https://cachefly.alfredapp.com/Alfred_3.0.2_676.zip" ;
 DOWNLOADED_FILE="Alfred.zip";
-APP_NAME="Alfred*.app";
+APP_NAME="Alfred 3.app";
 
 http --download --output ${DOWNLOADED_FILE} ${DOWNLOAD_URL};
 tar -xf ${DOWNLOADED_FILE};
